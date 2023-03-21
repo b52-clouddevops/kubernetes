e9b392167b7567af0f94ff69cc571f3dd5680798
CodingManoj <mvp2612@hotmail.com>
2023-03-20 20:10:17 -0400

    AutoPush

--- eks/main.tf
+++ eks/main.tf
@@ -1,8 +1,8 @@
 module "eks" {
   source             = "./vendor/modules/eks"
   ENV                = var.ENV
-  PRIVATE_SUBNET_IDS = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_ID
-  PUBLIC_SUBNET_IDS  = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_ID
+  PRIVATE_SUBNET_IDS = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
+  PUBLIC_SUBNET_IDS  = data.terraform_remote_state.vpc.outputs.PUBLIC_SUBNET_IDS
   DESIRED_SIZE       = 3
   MAX_SIZE           = 3
   MIN_SIZE           = 3
